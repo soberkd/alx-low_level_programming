aa70baf5d976f7251298f37ed69f32c51a9472b9
soberkd <soberkd6@gmail.com>
2022-09-08 11:07:34 +0000

    print f

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,11 +2,11 @@
 
 /**
  * main -Print some text
- * prints some text
+ * Return: Always 0 
  */
 
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
