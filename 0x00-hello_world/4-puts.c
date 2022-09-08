0ce6ea057b29e4fe6a267a493c0acb4b680847a4
soberkd <soberkd6@gmail.com>
2022-09-08 14:06:10 +0000

    Print my puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 /**
- * main - Prints  \"Programming is like building a multilingual puzzle
- * Mytext
- * Returns: Always 0
+ * main -Prints  \"Programming is like building a multilingual puzzle
+ *
+ * Return: my text or 0
  */
 
 int main(void)
