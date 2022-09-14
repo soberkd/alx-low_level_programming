92b8a67a27488355f3e23cb42181b449c992e10e
soberkd <soberkd6@gmail.com>
2022-09-14 11:07:30 +0000

    9 times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,4 +1,7 @@
 #include "main.h"
+/**
+ * times_table - prints table to 9
+ */
 
 void times_table(void)
 {
@@ -14,7 +17,7 @@ void times_table(void)
 			{
 				_putchar(',');
 				_putchar(' ');
-			} 
+			}
 			if (mult == 0)
 			{
 				_putchar('0');
