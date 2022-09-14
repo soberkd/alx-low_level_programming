d263064124dee817daeb6d67992e41a4378edb87
soberkd <soberkd6@gmail.com>
2022-09-14 12:02:55 +0000

    positive or negative

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <stddef.h>
+#include "main.h"
+/* more headers goes there */
+
+/**
+ * main - main fuction
+ *
+ * Return: value of n if positive or zero or negative or return 0
+ */
+
+int debug(void)
+{
+		int n;
+		/* your code goes there */
+		if (positive_or_negative(int i) == 0)
+		{
+			printf("Fuction is working");
+		}
+		else
+		{
+			printf("Error");
+		}
+		return (0);
+}
