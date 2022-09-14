763042ae5a8a3e7029e2dacb951802bcc25e177d
soberkd <soberkd6@gmail.com>
2022-09-14 11:04:26 +0000

    9 times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -10,15 +10,17 @@ void times_table(void)
 		for (mult = 0; mult <= 9; mult++)
 		{
 			square = base * mult;
-			if ( mult != 0)
+			if (mult != 0)
 			{
 				_putchar(',');
 				_putchar(' ');
-			}if (base == 0)
+			} 
+			if (mult == 0)
 			{
 				_putchar('0');
+
 			}
-			else if ((square < 10) && (base != 0))
+			else if ((square < 10) && (mult != 0))
 			{
 				_putchar(' ');
 				_putchar(square + '0');
