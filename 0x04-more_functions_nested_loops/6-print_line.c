615e597f1c0c8a06e5ee802a46383964b31eee65
soberkd <soberkd6@gmail.com>
2022-09-15 09:51:46 +0000

    Straight line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_line - prints a line
+ * @n: length of line
+ */
+void print_line(int n)
+{
+	int i;
+
+	if (n > 0)
+	{
+		for (i = 1; i <= n; i++)
+		{
+			_putchar('_');
+		}
+		_putchar('\n');
+	}
+	else
+		_putchar('\n');
+}
