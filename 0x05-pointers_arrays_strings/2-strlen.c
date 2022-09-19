99406885f797ac0b9a2ef37340fb8435ca40c00f
soberkd <soberkd6@gmail.com>
2022-09-19 09:57:32 +0000

    length of a strwqing

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strlen - determines the length of a string
+ * @s: string of which size to be determined
+ * Return: length of string
+ */
+int _strlen(char *s)
+{
+	return (strlen(s));
+}
