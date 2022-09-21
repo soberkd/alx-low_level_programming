61f60dbb6398152c73c52e0285946c2baab960bc
soberkd <soberkd6@gmail.com>
2022-09-21 16:11:23 +0000

    Reverse content of an array of integers

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -11,11 +11,11 @@ void reverse_array(int *a, int n)
 	int i, temp;
 
 	i = 0;
-	while (i < n/2)
+	while (i < n / 2)
 	{
 		temp = a[i];
-		a[i] = a[n-i-1];
-		a[n-i-1] = temp;
+		a[i] = a[n - i - 1];
+		a[n - i - 1] = temp;
 		i++;
 	}
 
