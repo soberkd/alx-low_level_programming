9d3abcaa07313bbd1164f6a6b2786fd5b225ec4a
soberkd <soberkd6@gmail.com>
2022-09-26 12:43:27 +0000

    Point arrays and strings

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -5,7 +5,7 @@
  * _strspn - gets the length of a prefix string
  * @s: string
  * @accept: length to be determined
- * Returns the number of bytes in the initial segment of s which consist only
+ * Return: the number of bytes in the initial segment of s which consist only
  *  of bytes from accept
  */
 unsigned int _strspn(char *s, char *accept)
