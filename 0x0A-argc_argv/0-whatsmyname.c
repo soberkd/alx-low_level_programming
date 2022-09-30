9c82479bee9cf71a72c5c17c4f8a724a61536847
soberkd <soberkd6@gmail.com>
2022-09-30 09:35:25 +0000

    Print name of program

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - prints name of program
+ * @argc: arguement count
+ * @argv: arguement vector
+ * Return: always 0
+ */
+int main(__attribute__((unused)) int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
