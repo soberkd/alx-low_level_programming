b63b4ca96f97f71b6714f2a0c74861b94c98122e
soberkd <soberkd6@gmail.com>
2022-10-04 08:40:42 +0000

    Pointer to a new memory

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _strdup - copies a string and returns an address to copied memory
+ * @str: string to be copied
+ * Return: duplicate of string or NULL if str is null
+ */
+char *_strdup(char *str)
+{
+	char *new_str;
+	unsigned int i = 0, j = 0;
+
+	while (str[i])
+		i++;
+	new_str = malloc(sizeof(char) * (i + 1));
+	if (new_str == NULL)
+		return (NULL);
+	while (str[j])
+	{
+		new_str[j] = str[j];
+		j++;
+	}
+	return (new_str);
+
+}
