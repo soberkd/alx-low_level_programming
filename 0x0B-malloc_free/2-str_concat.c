9f0c8f14070b54804c76b37f6df57dda15ce6a84
soberkd <soberkd6@gmail.com>
2022-10-04 10:28:23 +0000

    concatinate string

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -24,12 +24,13 @@ char *str_concat(char *s1, char *s2)
 	con = malloc(sizeof(char) * (j + i + 1));
 	if (con != NULL)
 	{
-		for (x = 0; x <= i; x++)
+		for (x = 0; x < i; x++)
 			con[x] = s1[x];
-		for (y = 0; y <= j; y++)
+		for (y = 0; y < j; y++)
 			con[x + y] = s2[y];
 	}
 	else
 		return (NULL);
+	con[x + y] = '\0';
 	return (con);
 }
