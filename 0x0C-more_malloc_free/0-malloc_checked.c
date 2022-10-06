99f39feaefb18da5611ac4112f64a432b04fb3ee
soberkd <soberkd6@gmail.com>
2022-10-06 09:44:50 +0000

    Allocate memory

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -3,12 +3,13 @@
 /**
  * malloc_checked - allocates memory using malloc
  * @b: size
+ * Return: Pointer to the allocated memory
  */
 void *malloc_checked(unsigned int b)
 {
 	void *ptr = malloc(b);
 
 	if (ptr == NULL)
-		exit (98);
+		exit(98);
 	return (ptr);
 }
