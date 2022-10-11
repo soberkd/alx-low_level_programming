a137ccc9e37ecfe5dfb17b29da997e6f0d5c7ccd
soberkd <soberkd6@gmail.com>
2022-10-11 15:53:46 +0000

    Initialize a structure

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -11,7 +11,12 @@
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	d->age = age;
-	d->name = name;
-	d->owner = owner;
+	if (d)
+	{
+		d->age = age;
+		d->name = name;
+		d->owner = owner;
+	}
+	else
+		return;
 }
