f876399e64e10775ec5eaf79f1cafe5dde04e7fc
soberkd <soberkd6@gmail.com>
2022-10-11 09:32:27 +0000

    Main header

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,9 @@
 #ifndef DOG_H
 #define DOG_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
 /**
  * struct dog - structure of dog
  * @age: age of the dog
@@ -14,4 +18,5 @@ struct dog
 	char *name;
 	char *owner;
 };
+void init_dog(struct dog *d, char *name, float age, char *owner);
 #endif
