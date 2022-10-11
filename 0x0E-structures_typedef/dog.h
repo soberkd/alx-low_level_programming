0364bcf7f3f215890066f02f20f238fabfff024d
soberkd <soberkd6@gmail.com>
2022-10-11 09:08:08 +0000

    Defining type struct dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - structure of dog
+ * @age: age of the dog
+ * @name: name of the dog
+ * @owner: ownwer o the dog
+ *
+ * Description: This structure stores infomation about the dog
+ */
+struct dog
+{
+	float age;
+	char *name;
+	char *owner;
+};
+#endif
