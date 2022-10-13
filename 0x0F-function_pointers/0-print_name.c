64116b6983ee2d488855de409f43cfaeba1bf9a7
soberkd <soberkd6@gmail.com>
2022-10-13 12:31:31 +0000

    Function that prints name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,12 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - fuction that prints name passed to it
+ * @name: name to be printed
+ * @f: pointer function
+ * @text
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
