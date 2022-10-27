2cdde0e11e57d82290d2ed6148d67a28d5762c46
soberkd <soberkd6@gmail.com>
2022-10-27 19:21:57 +0000

    Insert node

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -14,6 +14,12 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	listint_t *temp, *new;
 	unsigned int i = 1;
 
+	if (idx == 0)
+	{
+		new->next = *head;
+		*head = new;
+		return (new);
+	}
 	temp = *head;
 	while (temp)
 	{
