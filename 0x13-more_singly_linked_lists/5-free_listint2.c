ea233cd9e2fe646afad6ba396f305f39dce44914
soberkd <soberkd6@gmail.com>
2022-10-27 13:06:39 +0000

    Free list

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -8,15 +8,12 @@ void free_listint2(listint_t **head)
 {
 	listint_t *temp;
 
-	if (head == NULL)
-		*head = NULL;
-	if (*head == NULL)
-		*head = NULL;
 	while (*head)
 	{
 		temp = (*head)->next;
 		free(*head);
 		*head = temp;
 	}
+	free(*head)
 	*head = NULL;
 }
