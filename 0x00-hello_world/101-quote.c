754be8b37fd2aeaf34b35a44d76de2f5e269c502
soberkd <soberkd6@gmail.com>
2022-10-30 08:50:01 +0000

    Print a string

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -10,7 +10,7 @@
 int main(void)
 {
 	char name[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
-	/* write("and that piece of art is useful\" - Dora Korpar, 2015-10-19", 59); */
-	printf("%s \n", name);
+
+	printf("%s\n", name);
 	return (1);
 }
