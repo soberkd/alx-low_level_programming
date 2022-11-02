dfe5a5e2d8b31cfdc7028edaa6d9df85be622099
soberkd <soberkd6@gmail.com>
2022-11-02 12:17:18 +0000

    Create file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -14,12 +14,11 @@ int create_file(const char *filename, char *text_content)
 
 	if (!filename)
 		return (-1);
-	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
 	if (fd != -1)
 	{
 		write_out = write(fd, text_content, sizeof(text_content));
-		if (write_out == -1)
-			return (1);
+		return (1);
 	}
 	close(fd);
 	return (-1);
