6a844ae267e69b8aee6047aa8c032868c5a4ffb8
soberkd <soberkd6@gmail.com>
2022-11-02 19:04:53 +0000

    Append text at end of a file

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -13,7 +13,7 @@ int append_text_to_file(const char *filename, char *text_content)
 
 	if (!filename)
 		return (-1);
-	fd = open(filename, O_RDWR | O_APPEND);
+	fd = open(filename, O_WRONLY | O_APPEND);
 	if (fd)
 	{
 		if (!text_content)
@@ -24,6 +24,7 @@ int append_text_to_file(const char *filename, char *text_content)
 			close(fd);
 			return (-1);
 		}
+		close(fd);
 		return (1);
 	}
 	close(fd);
