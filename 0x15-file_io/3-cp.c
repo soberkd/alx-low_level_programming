2a996d09ebe9df3a09bb76cb8cce815d80863518
soberkd <soberkd6@gmail.com>
2022-11-03 20:16:45 +0000

    copy  file to another

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -9,48 +9,43 @@
  */
 int main(int argc, char *argv[])
 {
-	int fd1, fd2, wrt, rd, close_fd1, close_fd2;
+	int fd1, fd2, wrt, rd;
 	char buf[1024];
 
 	if (argc != 3)
 	{
-		dprintf(STDERR_FILENO, "Usage: %s file_from file_to\n"
-				, argv[0]);
+		dprintf(2, "Usage: %s file_from file_to\n", argv[0]);
 		exit(97);
 	}
 	fd1 = open(argv[1], O_RDONLY);
-	if (fd1 == -1)
+	rd = read(fd1, buf, 1024);
+	if (fd1 == -1 || rd == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n"
-				, argv[1]);
+		dprintf(2, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
 	fd2 = open(argv[2], O_CREAT | O_RDWR | O_TRUNC, 0664);
 	if (fd2 == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n"
-				, argv[2]);
+		dprintf(2, "Error: Can't read from file %s\n", argv[2]);
 		exit(98);
 	}
-	rd = read(fd1, buf, 1024);
 	wrt = write(fd2, buf, rd);
 	if (wrt == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 		exit(99);
 	}
-	close_fd1 = close(fd1);
-	if (close_fd1 == -1)
+	if (close(fd1) == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd1);
 		exit(100);
 	}
-	close_fd2 = close(fd2);
-	if (close_fd2 == -1)
+	if (close(fd2) == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd2);
 		exit(100);
 	}
 	return (0);
-
 }
+
