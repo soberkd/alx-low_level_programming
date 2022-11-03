0c6463f44a49b0a1153752974f5bc621e7513371
soberkd <soberkd6@gmail.com>
2022-11-03 14:59:48 +0000

    Append text to file

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -15,7 +15,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	if (!filename)
 		return (-1);
 	if (!text_content)
-		return (-1);
+		return (1);
 	fd = open(filename, O_WRONLY | O_APPEND, 0600);
 	if (fd == -1)
 		return (-1);
