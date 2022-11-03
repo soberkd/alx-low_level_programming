4c69db7a2a3fa232e251fd5ffc67d4affe75df67
soberkd <soberkd6@gmail.com>
2022-11-03 14:44:08 +0000

    Create a new file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <string.h>
 
 /**
  * create_file - fuction that creates a file
@@ -9,7 +10,7 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int fd, wrt;
+	int fd, wrt, len;
 
 	if (!filename)
 		return (-1);
@@ -17,8 +18,9 @@ int create_file(const char *filename, char *text_content)
 	if (fd == -1)
 		return (-1);
 
-	wrt = write(fd, text_content, sizeof(text_content));
-	if (wrt != sizeof(text_content))
+	len = strlen(text_content);
+	wrt = write(fd, text_content, len);
+	if (wrt != len)
 	{
 		close(fd);
 		return (-1);
